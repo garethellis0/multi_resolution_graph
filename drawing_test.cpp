ab485528942efa20a8e89764156e90796878300f
Gareth Ellis <gareth.ellis0@gmail.com>
2018-03-02 11:58:50 -0800

    changed the drawing test to represent a possible field

--- drawing_test.cpp
+++ drawing_test.cpp
@@ -36,8 +36,8 @@ cv::Mat drawGraphNode(GraphNode<T> node, int window_resolution){
             cv::Mat subNode = drawNode(*node.getSubNodes()[rowIndex][colIndex],
                              window_resolution/node.getResolution());
             cv::Rect roi(
-                    window_resolution/node.getResolution() * rowIndex,
                     window_resolution/node.getResolution() * colIndex,
+                    window_resolution/node.getResolution() * rowIndex,
                     window_resolution/node.getResolution(),
                     window_resolution/node.getResolution()
             );
@@ -64,8 +64,8 @@ void callBack(int event, int y, int x, int flags, void* userdata){
     if (event == CV_EVENT_LBUTTONUP) {
         // Convert window coordinates to graph coordinates
         Coordinates coordinates = {
-                (x / (double)window_resolution) * graphNode->getScale(),
                 (y / (double)window_resolution) * graphNode->getScale(),
+                (x / (double)window_resolution) * graphNode->getScale(),
         };
         graphNode->changeResolutionOfClosestNode(coordinates, 2);
         globalImage = drawNode(*graphNode, window_resolution);
@@ -79,28 +79,66 @@ void callBack(int event, int y, int x, int flags, void* userdata){
 int main(){
 
     GraphFactory<int> graph_factory;
-    graph_factory.setGraphScale(100);
+    graph_factory.setGraphScale(9);
     graph_factory.setGraphTopLevelResolution(1);
 
     Rectangle<int> rectangle =
             Rectangle<int>(0,0,(Coordinates){0,0});
-    rectangle = Rectangle<int>(10,10,(Coordinates){10,10});
-    graph_factory.setMaxScaleInArea(rectangle, 0.5);
-    rectangle = Rectangle<int>(10,10,(Coordinates){30,40});
-    graph_factory.setMaxScaleInArea(rectangle, 1);
-    rectangle = Rectangle<int>(30,10,(Coordinates){60,40});
-    graph_factory.setMaxScaleInArea(rectangle, 1);
-    rectangle = Rectangle<int>(10,60,(Coordinates){70,40});
-    graph_factory.setMaxScaleInArea(rectangle, 0.5);
-    rectangle = Rectangle<int>(5,10,(Coordinates){80,80});
-    graph_factory.setMaxScaleInArea(rectangle, 0.5);
+    // Base Field
+    rectangle = Rectangle<int>(6,9,(Coordinates){1.5,0});
+    graph_factory.setMaxScaleInArea(rectangle, 0.2);
+    // Defensive Areas
+    rectangle = Rectangle<int>(2,1,(Coordinates){3.5,0});
+    graph_factory.setMaxScaleInArea(rectangle, 0.1);
+    rectangle = Rectangle<int>(2,1,(Coordinates){3.5,8});
+    graph_factory.setMaxScaleInArea(rectangle, 0.1);
+//    rectangle = Rectangle<int>(3,2,(Coordinates){4.5,0});
+//    graph_factory.setMaxScaleInArea(rectangle, 0.1);
+//    rectangle = Rectangle<int>(1,1,(Coordinates){4.5,4.5});
+//    graph_factory.setMaxScaleInArea(rectangle, 0.05);
+//    rectangle = Rectangle<int>(1,1,(Coordinates){1,1});
+//    graph_factory.setMaxScaleInArea(rectangle, 0.05);
+//    rectangle = Rectangle<int>(1,1,(Coordinates){7,6});
+//    graph_factory.setMaxScaleInArea(rectangle, 0.05);
+    //rectangle = Rectangle<int>(10,10,(Coordinates){10,10});
+    //graph_factory.setMaxScaleInArea(rectangle, 0.5);
+    //rectangle = Rectangle<int>(10,10,(Coordinates){30,40});
+    //graph_factory.setMaxScaleInArea(rectangle, 1);
+    //rectangle = Rectangle<int>(30,10,(Coordinates){60,40});
+    //graph_factory.setMaxScaleInArea(rectangle, 1);
+    //rectangle = Rectangle<int>(10,60,(Coordinates){70,40});
+    //graph_factory.setMaxScaleInArea(rectangle, 0.5);
+    //rectangle = Rectangle<int>(5,10,(Coordinates){80,80});
+    //graph_factory.setMaxScaleInArea(rectangle, 0.5);
 
     Circle<int> circle =
             Circle<int>(0,(Coordinates){0,0});
-    circle = Circle<int>(10,(Coordinates){40,40});
-    graph_factory.setMaxScaleInArea(circle, 0.5);
-    circle = Circle<int>(10,(Coordinates){70,70});
-    graph_factory.setMaxScaleInArea(circle, 0.5);
+    // Robots
+    circle = Circle<int>(0.2,(Coordinates){7,3});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){3,7});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){4.7,7.8});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){4.2,0.6});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){3,2});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){3.7,2.7});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){4.5,2.4});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){6,4});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){5.4,3.8});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){5,6});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){5.4,6.3});
+    circle = Circle<int>(0.2,(Coordinates){4.8,6.4});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
+    circle = Circle<int>(0.2,(Coordinates){3,4.5});
+    graph_factory.setMaxScaleInArea(circle, 0.05);
 
     /*
     std::vector<std::pair<Area<int>*, double>> areas_and_scales;
@@ -148,6 +186,10 @@ int main(){
     std::cout << "Done generating" << std::endl;
     std::cout << "Time to generate graph = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() <<std::endl;
     graphNode = &graphNode_val;
+
+    // Figure out how many nodes we generated
+    auto all_nodes = graphNode->getAllSubNodes();
+    std::cout << "We generated this many nodes: " << all_nodes.size() << std::endl;
 
     // Draw the graph over the cv::Mat
     globalImage = drawNode(*graphNode, window_resolution);
